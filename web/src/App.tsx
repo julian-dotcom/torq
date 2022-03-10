dc08614160240c40d1ba7c38b429d5a90a91f1a6
Max Edwards <youwontforgetthis@gmail.com>
2022-03-10 17:08:26 +0000

    added react router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
@@ -7,10 +8,13 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
+                <div>
+                    <h1>Welcome to React Router!</h1>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="about" element={<About />} />
+                    </Routes>
+                </div>                <a
                     className="App-link"
                     href="https://reactjs.org"
                     target="_blank"
@@ -20,6 +24,37 @@ function App() {
                 </a>
             </header>
         </div>
+    );
+}
+
+function Home() {
+    return (
+        <>
+            <main>
+                <h2>Welcome to the homepage!</h2>
+                <p>You can do this, I believe in you.</p>
+            </main>
+            <nav>
+                <Link to="/about">About</Link>
+            </nav>
+        </>
+    );
+}
+
+function About() {
+    return (
+        <>
+            <main>
+                <h2>Who are we?</h2>
+                <p>
+                    That feels like an existential question, don't you
+                    think?
+                </p>
+            </main>
+            <nav>
+                <Link to="/">Home</Link>
+            </nav>
+        </>
     );
 }
 
