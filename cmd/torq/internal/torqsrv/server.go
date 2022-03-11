04cc6b3d63c29aea1d09335d5525ea6e5f94e07e
Max Edwards <youwontforgetthis@gmail.com>
2022-03-11 14:14:34 +0000

    gin serves react website

--- cmd/torq/internal/torqsrv/server.go
+++ cmd/torq/internal/torqsrv/server.go
@@ -6,10 +6,33 @@ import (
 
 func Start(port int) {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	registerRoutes(r)
 	r.Run()
+}
+
+func registerRoutes(r *gin.Engine) {
+	registerStaticRoutes(r)
+	api := r.Group("/api")
+	{
+		api.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+}
+
+func registerStaticRoutes(r *gin.Engine) {
+	r.NoRoute(func(c *gin.Context) {
+		c.File("./web/build/index.html")
+	})
+	r.StaticFile("/", "./web/build/index.html")
+	r.StaticFile("/favicon.ico", "./web/build/favicon.ico")
+	r.StaticFile("/logo192.png", "./web/build/logo192.png")
+	r.StaticFile("/logo512.png", "./web/build/logo512.png")
+	r.StaticFile("/manifest.json", "./web/build/manifest.json")
+	r.StaticFile("/robots.txt", "./web/build/robots.txt")
+	r.StaticFile("/asset-manifest.json", "./web/build/asset-manifest.json")
+	r.Static("/static", "./web/build/static")
+
 }
