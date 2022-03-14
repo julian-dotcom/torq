6569d003ffc519ae3acb4fc0d3f3f93dc738e586
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2022-03-14 19:17:29 +0100

    Implements table cells

--- web/src/components/table/Table.tsx
+++ web/src/components/table/Table.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './table.scss'
+import tableRow from "./TableRow";
+import NameCell from "./cells/NameCell";
+import NumericCell from "./cells/NumericCell";
+import BarCell from "./cells/BarCell";
 
 export interface ColumnMetaData {
   primary: string;
@@ -9,165 +13,205 @@ export interface ColumnMetaData {
   align?: string;
 }
 
-const headers = {
-  "group_name": {primary: '', secondary: 'Name', type: ''},
-  "amount_out": {primary: 'Amount', secondary: 'Outbound', type: ''},
-  "amount_in": {primary: 'Amount', secondary: 'Inbound', type: ''},
-  "amount_total": {primary: 'Amount', secondary: 'Total', type: ''},
-  "revenue_out": {primary: 'Revenue', secondary: 'Outbound', type: ''},
-  "revenue_in": {primary: 'Revenue', secondary: 'Inbound', type: ''},
-  "revenue_total": {primary: 'Revenue', secondary: 'Total', type: ''},
-  "count_out": {primary: 'Count', secondary: 'Outbound', type: ''},
-  "count_in": {primary: 'Count', secondary: 'Inbound', type: ''},
-  "count_total": {primary: 'Count', secondary: 'Total', type: ''},
-  "capacity": {primary: '', secondary: 'Capacity', type: ''},
-  "turnover": {primary: '', secondary: 'Turnover', type: ''},
+interface ColumnMeta {
+  primaryHeading: string,
+  secondaryHeading: string,
+  type?: string,
+  key: string,
+  width?: number
 }
 
-let currentRows = [
-    {
-      group_name: "LNBig",
-      amount_out: 1,
-      amount_in: 1,
-      amount_total: 1,
-      revenue_out: 1,
-      revenue_in: 1,
-      revenue_total: 1,
-      count_out: 1,
-      count_in: 1,
-      count_total: 1,
-      capacity: 1,
-      turnover: 1,
-    },    {
-      group_name: "LNBig",
-      amount_out: 1,
-      amount_in: 1,
-      amount_total: 1,
-      revenue_out: 1,
-      revenue_in: 1,
-      revenue_total: 1,
-      count_out: 1,
-      count_in: 1,
-      count_total: 1,
-      capacity: 1,
-      turnover: 1,
-    },    {
-      group_name: "LNBig",
-      amount_out: 1,
-      amount_in: 1,
-      amount_total: 1,
-      revenue_out: 1,
-      revenue_in: 1,
-      revenue_total: 1,
-      count_out: 1,
-      count_in: 1,
-      count_total: 1,
-      capacity: 1,
-      turnover: 1,
-    },    {
-      group_name: "LNBig",
-      amount_out: 1,
-      amount_in: 1,
-      amount_total: 1,
-      revenue_out: 1,
-      revenue_in: 1,
-      revenue_total: 1,
-      count_out: 1,
-      count_in: 1,
-      count_total: 1,
-      capacity: 1,
-      turnover: 1,
-    },
+const columns: ColumnMeta[] = [
+  {primaryHeading: '', secondaryHeading: 'Name', type: 'NameCell', key: 'group_name'},
+  {primaryHeading: '', secondaryHeading: 'Capacity', type:'NumericCell', key: 'capacity'},
+  {primaryHeading: '', secondaryHeading: 'Turnover', type:'NumericCell', key: 'turnover'},
+  {primaryHeading: 'Forwarded Amount', secondaryHeading: 'Outbound', type:'BarCell', key: 'amount_out'},
+  {primaryHeading: 'Forwarded Amount', secondaryHeading: 'Inbound', type:'NumericCell', key: 'amount_in'},
+  {primaryHeading: 'Forwarded Amount', secondaryHeading: 'Total', type:'NumericCell', key: 'amount_total'},
+  {primaryHeading: 'Forwarding Revenue', secondaryHeading: 'Outbound', type:'NumericCell', key: 'revenue_out'},
+  {primaryHeading: 'Forwarding Revenue', secondaryHeading: 'Inbound', type:'NumericCell', key: 'revenue_in'},
+  {primaryHeading: 'Forwarding Revenue', secondaryHeading: 'Total', type:'NumericCell', key: 'revenue_total'},
+  {primaryHeading: 'Successfull Forwards', secondaryHeading: 'Outbound', type:'NumericCell', key: 'count_out'},
+  {primaryHeading: 'Successfull Forwards', secondaryHeading: 'Inbound', type:'NumericCell', key: 'count_in'},
+  {primaryHeading: 'Successfull Forwards', secondaryHeading: 'Total', type:'NumericCell', key: 'count_total'},
 ];
 
-let previousRows = [
-    {
-      group_name: "LNBig",
-      amount_out: 1,
-      amount_in: 1,
-      amount_total: 1,
-      revenue_out: 1,
-      revenue_in: 1,
-      revenue_total: 1,
-      count_out: 1,
-      count_in: 1,
-      count_total: 1,
-      capacity: 1,
-      turnover: 1,
-    },    {
-      group_name: "LNBig",
-      amount_out: 1,
-      amount_in: 1,
-      amount_total: 1,
-      revenue_out: 1,
-      revenue_in: 1,
-      revenue_total: 1,
-      count_out: 1,
-      count_in: 1,
-      count_total: 1,
-      capacity: 1,
-      turnover: 1,
-    },    {
-      group_name: "LNBig",
-      amount_out: 1,
-      amount_in: 1,
-      amount_total: 1,
-      revenue_out: 1,
-      revenue_in: 1,
-      revenue_total: 1,
-      count_out: 1,
-      count_in: 1,
-      count_total: 1,
-      capacity: 1,
-      turnover: 1,
-    },    {
-      group_name: "LNBig",
-      amount_out: 1,
-      amount_in: 1,
-      amount_total: 1,
-      revenue_out: 1,
-      revenue_in: 1,
-      revenue_total: 1,
-      count_out: 1,
-      count_in: 1,
-      count_total: 1,
-      capacity: 1,
-      turnover: 1,
-    },
+interface RowType {
+  group_name: string,
+  amount_out: number,
+  amount_in: number,
+  amount_total: number,
+  revenue_out: number,
+  revenue_in: number,
+  revenue_total: number,
+  count_out: number,
+  count_in: number,
+  count_total: number,
+  capacity: number,
+  turnover: number,
+}
 
+let totalRows: RowType = {
+    group_name: "Total",
+    amount_out: 1200000,
+    amount_in: 1200000,
+    amount_total: 1200000,
+    revenue_out: 1200000,
+    revenue_in: 1200000,
+    revenue_total: 1200000,
+    count_out: 1200000,
+    count_in: 1200000,
+    count_total: 1200000,
+    capacity: 1200000,
+    turnover: 1.42,
+}
+let currentRows: RowType[] = [
+  {
+    group_name: "LNBig",
+    amount_out: 1200000,
+    amount_in: 1200000,
+    amount_total: 1200000,
+    revenue_out: 1200000,
+    revenue_in: 1200000,
+    revenue_total: 1200000,
+    count_out: 1200000,
+    count_in: 1200000,
+    count_total: 1200000,
+    capacity: 1200000,
+    turnover: 1.42,
+  },    {
+    group_name: "LNBig",
+    amount_out: 1,
+    amount_in: 1,
+    amount_total: 1,
+    revenue_out: 1,
+    revenue_in: 1,
+    revenue_total: 1,
+    count_out: 1,
+    count_in: 1,
+    count_total: 1,
+    capacity: 1,
+    turnover: 1,
+  },    {
+    group_name: "LNBig",
+    amount_out: 1,
+    amount_in: 1,
+    amount_total: 1,
+    revenue_out: 1,
+    revenue_in: 1,
+    revenue_total: 1,
+    count_out: 1,
+    count_in: 1,
+    count_total: 1,
+    capacity: 1,
+    turnover: 1,
+  },    {
+    group_name: "LNBig",
+    amount_out: 1,
+    amount_in: 1,
+    amount_total: 1,
+    revenue_out: 1,
+    revenue_in: 1,
+    revenue_total: 1,
+    count_out: 1,
+    count_in: 1,
+    count_total: 1,
+    capacity: 1,
+    turnover: 1,
+  },
+];
+let pastRow: RowType[] = [
+  {
+    group_name: "LNBig",
+    amount_out: 1,
+    amount_in: 1,
+    amount_total: 1,
+    revenue_out: 1,
+    revenue_in: 1,
+    revenue_total: 1,
+    count_out: 1,
+    count_in: 1,
+    count_total: 1,
+    capacity: 1,
+    turnover: 1,
+  },    {
+    group_name: "LNBig",
+    amount_out: 1,
+    amount_in: 1,
+    amount_total: 1,
+    revenue_out: 1,
+    revenue_in: 1,
+    revenue_total: 1,
+    count_out: 1,
+    count_in: 1,
+    count_total: 1,
+    capacity: 1,
+    turnover: 1,
+  },    {
+    group_name: "LNBig",
+    amount_out: 1,
+    amount_in: 1,
+    amount_total: 1,
+    revenue_out: 1,
+    revenue_in: 1,
+    revenue_total: 1,
+    count_out: 1,
+    count_in: 1,
+    count_total: 1,
+    capacity: 1,
+    turnover: 1,
+  },    {
+    group_name: "LNBig",
+    amount_out: 1,
+    amount_in: 1,
+    amount_total: 1,
+    revenue_out: 1,
+    revenue_in: 1,
+    revenue_total: 1,
+    count_out: 1,
+    count_in: 1,
+    count_total: 1,
+    capacity: 1,
+    turnover: 1,
+  },
 ];
 
-function HeaderCell(key: string, primary :string, secondary:string) {
+function HeaderCell(item: ColumnMeta) {
   return (
-    <div className={"header"}>
-      <div className="top">{primary}</div>
-      <div className="bottom">{secondary}</div>
+    <div className={"header " + item.key} key={item.key}>
+      <div className="top">{item.primaryHeading}</div>
+      <div className="bottom">{item.secondaryHeading}</div>
     </div>
   )
 }
 
 function Table() {
-    let key: keyof typeof headers
+    let key: keyof typeof columns
     let channel: keyof typeof currentRows
     return (
       <div className="table-wrapper">
         <style>
-          {".table-content {grid-template-columns: repeat("+Object.keys(headers).length+", minmax(100px,  1fr))}"}
+          {".table-content {grid-template-columns: repeat("+Object.keys(columns).length+",  fit-content(200px))}"}
         </style>
         <div className="table-content">
-          {Object.entries(headers).map(([key, item]) => {
-            console.log(key)
-            return  HeaderCell(key, item.primary,item.secondary)
-
+          {columns.map((item) => {
+            return  HeaderCell(item)
           })}
 
-          {currentRows.map((row, index) => {
-            return Object.entries(row).map(([key, cell]) => {
-              return (
-                <div className={"cell " + key} key={key}>
-                  {cell}
-                </div>
-              )
+          {currentRows.map((currentRow, index) => {
+            return columns.map((column) => {
+              let key = column.key as keyof RowType
+              let past = pastRow[index][key]
+              switch (column.type) {
+                case 'NameCell':
+                  return NameCell((currentRow[key] as string), key, index)
+                case 'NumericCell':
+                  return NumericCell((currentRow[key] as number), (past as number), key, index)
+                case "BarCell":
+                  return BarCell((currentRow[key] as number), (totalRows[key] as number), (past as number), key, index)
+                default:
+                  return NumericCell((currentRow[key] as number), (past as number), key, index)
+              }
             })
           })}
 
