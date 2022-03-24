4a3ed156f84640bd90139b2d025f7f1b73daf2ca
Max Edwards <youwontforgetthis@gmail.com>
2022-03-24 22:17:53 +0000

    working filters for the frontend

--- web/src/filter.ts
+++ web/src/filter.ts
@@ -1,48 +1,75 @@
+// just some sample data. the below code makes the assumption that the data will be an array of objects
 const data = [
   {
     name: "Alice",
-    age: "50",
+    age: 50,
     tags: ["clever", "astute"]
   },
   {
     name: "Bob",
-    age: "20",
+    age: 20,
     tags: ["kind"]
   }
 ]
 
+// interface for a filter that will filter the data
 interface FilterFunc {
-  (input: Array, key: string, parameter: any): Array;
+  (input: Array<any>, key: string, parameter: any): Array<any>;
 }
 
+// interface for some metadata about a filter function such as what datatype it operates on and it's name
 interface FilterFuncObj {
   name: string,
   type: string,
   func: FilterFunc,
 }
 
+// available filter types that can be picked in the UI and what they do
 const filterFuncs: Array<FilterFuncObj> = [
   {
     name: "equals",
     type: "number",
-    func: (input: Array<any>, key: string, parameter: number) => input.filter(item => item["key"] === parameter)
+    func: (input: Array<any>, key: string, parameter: number) => input.filter(item => item[key] === parameter)
+  },
+  {
+    name: "matches",
+    type: "string",
+    func: (input: Array<any>, key: string, parameter: number) => input.filter(item => item[key].includes(parameter))
+  },
+  {
+    name: "includes",
+    type: "tag",
+    func: (input: Array<any>, key: string, parameter: number) => input.filter(item => item[key].some(tag => tag === parameter))
   },
 ]
 
+// an interface for a configured filter
 interface filter {
   filterFunc: FilterFuncObj,
   key: string,
   paramter: any
 }
 
+// these are the filters and the arguments that the user has picked in the UI
 let filters: Array<filter> = [
   {
     filterFunc: filterFuncs[0],
     key: "age",
     paramter: 50
-  }
+  },
+  {
+    filterFunc: filterFuncs[1],
+    key: "name",
+    paramter: "lice"
+  },
+  {
+    filterFunc: filterFuncs[2],
+    key: "tags",
+    paramter: "astute"
+  },
 ]
 
+// this takes all the collected filters and applies them one by one to the original data
 const new_data = filters.reduce((prev, cur) => cur.filterFunc.func(prev, cur.key, cur.paramter), data)
 
 console.log(new_data)
