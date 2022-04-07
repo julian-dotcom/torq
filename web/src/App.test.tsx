3a7b726e81640e6231c712a9ed0f95fc60fb6076
Max Edwards <youwontforgetthis@gmail.com>
2022-04-07 13:10:48 +0100

    fixed the broken app.text.tsx test

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders login page', () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>);
+  const elemane = screen.getByText(/login/i);
+  expect(elemane).toBeInTheDocument();
 });
