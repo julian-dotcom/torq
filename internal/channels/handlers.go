3a4b03b9f7a35a63c06016131509bd47fa307722
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2022-04-21 17:40:41 +0200

    All channels included in channels query

--- internal/channels/handlers.go
+++ internal/channels/handlers.go
@@ -34,40 +34,45 @@ func getChannelsHandler(c *gin.Context, db *sqlx.DB) {
 }
 
 type channelData struct {
+	// Alias of remote peer
+	Alias null.String `json:"alias"`
 	// Database primary key of channel
 	ChannelDBID null.Int `json:"channelDbId"`
+	// The channel point
+	ChanPoint null.String `json:"channel_point"`
+	// The remote public key
+	PubKey null.String `json:"pub_key"`
 	// Short channel id in c-lightning / BOLT format
 	ShortChannelID null.String `json:"shortChannelId"`
 	// The channel ID
 	ChanId uint64 `json:"chan_id"`
-	// Alias of remote peer
-	Alias null.String `json:"alias"`
-	// The remote public key
-	PubKey null.String `json:"pub_key"`
+	// Color of remote peer (Vanity)
+	Color null.String `json:"color"`
+	// The channels total capacity (as created)
+	Capacity uint64 `json:"capacity"`
+
+	// The  outbound amount in sats (Satoshis)
+	AmountOut uint64 `json:"amount_out"`
 	// The inbound amount in sats (Satoshis)
 	AmountIn uint64 `json:"amount_in"`
+	// The total amount in sats (Satoshis) forwarded
+	AmountTotal uint64 `json:"amount_total"`
+
+	// The outbound revenue in sats. This is what the channel has directly produced.
+	RevenueOut uint64 `json:"revenue_out"`
 	// The inbound revenue in sats. This is what the channel has indirectly produced.
 	// This revenue are not really earned by this channel/peer/group, but represents
 	// the channel/peer/group contribution to revenue earned by other channels.
 	RevenueIn uint64 `json:"revenue_in"`
-	// Number of inbound forwards.
-	CountIn uint64 `json:"count_in"`
-	// The  utbound amount in sats (Satoshis)
-	AmountOut uint64 `json:"amount_out"`
-	// The outbound revenue in sats. This is what the channel has directly produced.
-	RevenueOut uint64 `json:"revenue_out"`
-	// Number of outbound forwards.
-	CountOut uint64 `json:"count_out"`
-
-	// The total amount in sats (Satoshis) forwarded
-	AmountTotal uint64 `json:"amount_total"`
 	// The total revenue in sats. This is what the channel has directly and indirectly produced.
 	RevenueTotal uint64 `json:"revenue_total"`
+
+	// Number of outbound forwards.
+	CountOut uint64 `json:"count_out"`
+	// Number of inbound forwards.
+	CountIn uint64 `json:"count_in"`
 	// Number of total forwards.
 	CountTotal uint64 `json:"count_total"`
-
-	// The channels total capacity (as created)
-	Capacity uint64 `json:"capacity"`
 
 	TurnoverOut   float32 `json:"turnover_out"`
 	TurnoverIn    float32 `json:"turnover_in"`
@@ -77,88 +82,77 @@ type channelData struct {
 func getAggForwardsByChanIds(db *sqlx.DB, fromTime time.Time, toTime time.Time) (r []*channelData, err error) {
 	var sql = `
 select
-    channel.channel_db_id,
-	channel.short_channel_id,
-    coalesce(fwr.chan_id, 0) as chan_id,
     coalesce(ne.alias, '') as alias,
-    fwr.pub_key,
+    c.channel_db_id,
+    ce.chan_point,
+    ce.pub_key,
+    c.short_channel_id,
+    coalesce(ce.chan_id, 0) as chan_id,
+    ne.color,
+    coalesce(ce.capacity::numeric, 0) as capacity,
 
-    fwr.amount_out,
-    fwr.amount_in,
-    (fwr.amount_in + fwr.amount_out) as amount_total,
+    coalesce(fw.amount_out, 0) as amount_out,
+    coalesce(fw.amount_in, 0) as amount_in,
+    coalesce((fw.amount_in + fw.amount_out), 0) as amount_total,
 
-    fwr.revenue_out,
-    fwr.revenue_in,
-    (fwr.revenue_in + fwr.revenue_out) as revenue_total,
+    coalesce(fw.revenue_out, 0) as revenue_out,
+    coalesce(fw.revenue_in, 0) as revenue_in,
+    coalesce((fw.revenue_in + fw.revenue_out), 0) as revenue_total,
 
-    fwr.count_out,
-    fwr.count_in,
-    (fwr.count_in + fwr.count_out) as count_total,
+    coalesce(fw.count_out, 0) as count_out,
+    coalesce(fw.count_in, 0) as count_in,
+    coalesce((fw.count_in + fw.count_out), 0) as count_total,
 
-    coalesce(fwr.capacity, 0),
-
-    coalesce(round(fwr.amount_out / fwr.capacity, 2), 0) as turnover_out,
-    coalesce(round(fwr.amount_in / fwr.capacity, 2), 0) as turnover_in,
-    coalesce(round((fwr.amount_in + fwr.amount_out) / fwr.capacity, 2), 0) as turnover_total
-
-from (
-    select ce.pub_key,
-	    ce.chan_point,
-        fw.chan_id,
-        ce.closed,
-        ce.capacity,
-        amount_out,
-        amount_in,
-        revenue_out,
-        revenue_in,
-        count_out,
-        count_in
-    from (
-        select coalesce(o.chan_id, i.chan_id, 0) as chan_id,
-               coalesce(o.amount,0) as amount_out,
-               coalesce(o.revenue,0) as revenue_out,
-               coalesce(o.count,0) as count_out,
-               coalesce(i.amount,0) as amount_in,
-               coalesce(i.revenue,0) as revenue_in,
-               coalesce(i.count,0) as count_in
-        from (
-            select outgoing_channel_id chan_id,
-                   floor(sum(outgoing_amount_msat)/1000) as amount,
-                   floor(sum(fee_msat)/1000) as revenue,
-                   count(time) as count
-            from forward
-            where time >= $1
-                and time <= $2
-            group by outgoing_channel_id
-            ) as o
-        full outer join (
-            select incoming_channel_id as chan_id,
-                   floor(sum(incoming_amount_msat)/1000) as amount,
-                   floor(sum(fee_msat)/1000) as revenue,
-                   count(time) as count
-            from forward
-            where time >= $1
-                and time <= $2
-            group by incoming_channel_id) as i
-        on i.chan_id = o.chan_id
-    ) as fw
-    left join (
-        select chan_id,
-               pub_key,
-		       chan_point,
-               last(event->'capacity', time)::numeric as capacity,
-               last(event_type, time) = 1 as closed
-        from channel_event
-        where event_type in (0,1)
-        group by chan_id, pub_key, chan_point
-    ) as ce
-    on ce.chan_id = fw.chan_id
-) as fwr
+    coalesce(round(fw.amount_out / ce.capacity::numeric, 2), 0) as turnover_out,
+    coalesce(round(fw.amount_in / ce.capacity::numeric, 2), 0) as turnover_in,
+    coalesce(round((fw.amount_in + fw.amount_out) / ce.capacity::numeric, 2), 0) as turnover_total
+from channel as c
 left join (
-    select pub_key, last(alias, timestamp) as alias from node_event group by pub_key) as ne
-on ne.pub_key = fwr.pub_key
-left join channel on channel.channel_point = fwr.chan_point
-order by revenue_out desc;
+    select
+        distinct chan_id,
+        chan_point,
+        pub_key,
+        last(event->'capacity', time) as capacity
+    from channel_event where event_type in (0,1)
+   group by chan_id, chan_point, pub_key
+) as ce on c.channel_point = ce.chan_point
+left join (
+    select
+        pub_key,
+        last(alias, timestamp) as alias,
+        last(color, timestamp) as color
+    from node_event
+    group by pub_key
+) as ne on ce.pub_key = ne.pub_key
+left join (
+    select coalesce(o.chan_id, i.chan_id, 0) as chan_id,
+        coalesce(o.amount,0) as amount_out,
+        coalesce(o.revenue,0) as revenue_out,
+        coalesce(o.count,0) as count_out,
+        coalesce(i.amount,0) as amount_in,
+        coalesce(i.revenue,0) as revenue_in,
+        coalesce(i.count,0) as count_in
+    from (
+        select outgoing_channel_id chan_id,
+               floor(sum(outgoing_amount_msat)/1000) as amount,
+               floor(sum(fee_msat)/1000) as revenue,
+               count(time) as count
+        from forward
+        where time >= $1
+            and time <= $2
+        group by outgoing_channel_id
+        ) as o
+    full outer join (
+        select incoming_channel_id as chan_id,
+               floor(sum(incoming_amount_msat)/1000) as amount,
+               floor(sum(fee_msat)/1000) as revenue,
+               count(time) as count
+        from forward
+        where time >= $1
+            and time <= $2
+        group by incoming_channel_id) as i
+    on i.chan_id = o.chan_id
+) as fw on fw.chan_id = ce.chan_id;
 `
 
 	rows, err := db.Query(sql, fromTime, toTime)
@@ -169,21 +163,28 @@ order by revenue_out desc;
 	for rows.Next() {
 		c := &channelData{}
 		err = rows.Scan(
+			&c.Alias,
 			&c.ChannelDBID,
+			&c.ChanPoint,
+			&c.PubKey,
 			&c.ShortChannelID,
 			&c.ChanId,
-			&c.Alias,
-			&c.PubKey,
+			&c.Color,
+
+			&c.Capacity,
+
 			&c.AmountOut,
 			&c.AmountIn,
 			&c.AmountTotal,
+
 			&c.RevenueOut,
 			&c.RevenueIn,
 			&c.RevenueTotal,
+
 			&c.CountOut,
 			&c.CountIn,
 			&c.CountTotal,
-			&c.Capacity,
+
 			&c.TurnoverOut,
 			&c.TurnoverIn,
 			&c.TurnoverTotal,
