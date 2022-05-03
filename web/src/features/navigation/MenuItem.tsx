dde3a75bfb1a201a7ce362aca3b9a9ff021fbdfb
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2022-05-03 13:19:48 +0200

    switch menu-item to moduled css

--- web/src/features/navigation/MenuItem.tsx
+++ web/src/features/navigation/MenuItem.tsx
@@ -1,47 +1,65 @@
-import React from 'react';
-import './menu_item.scss'
-
+import React from "react";
+import styles from "./menu_item.module.scss";
+import classNames from "classnames";
 
 function MenuItem(props: {
-  text: string,
-  selected?: boolean,
-  icon?: any,
-  children?: any,
-  actions?: any,
-  routeTo?: string,
+  text: string;
+  selected?: boolean;
+  icon?: any;
+  children?: any;
+  actions?: any;
+  routeTo?: string;
 }) {
-
-  let TitleComponent = function(routeTo?: string) {
+  let TitleComponent = function (routeTo?: string) {
     if (routeTo) {
-      return (<a href={routeTo}  className={props.icon ? "title" : "title no-icon"} >
-        <div className="icon">{props.icon}</div>
-        <div className="text">{props.text}</div>
-      </a>)
+      return (
+        <a
+          href={routeTo}
+          className={classNames(styles.title, {
+            [styles.noIcon]: !!props.icon,
+          })}
+        >
+          <div className={styles.icon}>{props.icon}</div>
+          <div className={styles.text}>{props.text}</div>
+        </a>
+      );
     } else {
-      return (<div  className={props.icon ? "title" : "title no-icon"} >
-        <div className="icon">{props.icon}</div>
-        <div className="text">{props.text}</div>
-      </div>)
+      return (
+        <div
+          className={classNames(styles.title, {
+            [styles.noIcon]: !!props.icon,
+          })}
+        >
+          <div className={styles.icon}>{props.icon}</div>
+          <div className={styles.text}>{props.text}</div>
+        </div>
+      );
     }
-
-  }
+  };
 
   return (
-      <div className={"item " +  (props.selected ? "selected" : "")}>
+    <div
+      className={classNames(styles.item, { [styles.selected]: props.selected })}
+    >
+      <div className={styles.contentWrapper}>
+        {TitleComponent(props.routeTo)}
 
-          <div className="content-wrapper">
-
-            {TitleComponent(props.routeTo)}
-
-            {props.actions && (<div className="actions">
-              <div className="icon action">{props.actions}</div>
-            </div>)}
-
+        {props.actions && (
+          <div className={styles.actions}>
+            <div className={classNames(styles.icon, styles.action)}>
+              {props.actions}
+            </div>
           </div>
-
-        {props.children && (<div className="item sub">{props.children}</div>)}
+        )}
       </div>
-    );
+
+      {props.children && (
+        <div className={classNames(styles.item, styles.sub)}>
+          {props.children}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default MenuItem;
