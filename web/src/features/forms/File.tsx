41f786018f8d3e72bb5d964b04ec1b0374d974c6
Max Edwards <youwontforgetthis@gmail.com>
2022-05-21 13:15:01 +0100

    drag and drop handlers added to file upload

--- web/src/features/forms/File.tsx
+++ web/src/features/forms/File.tsx
@@ -1,16 +1,97 @@
+// Drag and drop!!
+// https://www.youtube.com/watch?v=vJG9lnO7jOM
+
 import styles from "./file.module.scss";
+import React from "react";
+import classNames from "classnames";
 
 interface fileProps {
   label?: string;
+  onFileDropped?: Function;
 }
-function File({ label }: fileProps) {
+function File({ label, onFileDropped }: fileProps) {
+  const drop = React.useRef<HTMLDivElement>(null);
+  const defaultMessageValue = "Drag file here or click to select";
+
+  const [message, setMessage] = React.useState<string | (string | React.ReactElement)[]>(defaultMessageValue);
+  const [dragging, setDragging] = React.useState(false);
+  const [fileError, setFileError] = React.useState(false);
+
+  React.useEffect(() => {
+    drop.current?.addEventListener("dragover", handleDragOver);
+    drop.current?.addEventListener("drop", handleDrop);
+    drop.current?.addEventListener("dragenter", handleDragEnter);
+    drop.current?.addEventListener("dragleave", handleDragLeave);
+    return () => {
+      drop.current?.removeEventListener("dragover", handleDragOver);
+      drop.current?.removeEventListener("drop", handleDrop);
+      drop.current?.removeEventListener("dragenter", handleDragEnter);
+      drop.current?.removeEventListener("dragleave", handleDragLeave);
+    };
+  }, []);
+
+  const handleDragOver = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDrop = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setDragging(false);
+    setFileError(false);
+    if (e.dataTransfer) {
+      const files = e.dataTransfer.files;
+
+      if (files && files.length) {
+        if (files.length > 1) {
+          setMessage(["Too many files", <br key="br1" />, <br key="br2" />, "Drop file or click to select"]);
+          setFileError(true);
+          return;
+        }
+        onFileDropped && onFileDropped(files);
+        setMessage([
+          "Current file: " + files[0].name,
+          <br key="br1" />,
+          <br key="br2" />,
+          "To change, drop file or click to select",
+        ]);
+        console.log(files);
+      }
+    }
+  };
+
+  const handleDragEnter = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setDragging(true);
+  };
+
+  const handleDragLeave = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (e.target === drop.current) {
+      setDragging(false);
+    }
+  };
+
   return (
     <>
       <div style={{ marginBottom: "var(--form-label-margin-bottom)" }}>
         <span>{label}</span>
       </div>
-      <div className={styles.fileDropArea}>
-        <span>Drop files here or click to select</span>
+      <div
+        ref={drop}
+        className={classNames(
+          styles.fileDropArea,
+          { [styles.dragging]: dragging },
+          { [styles.error]: !dragging && fileError }
+        )}
+      >
+        <span style={{ textAlign: "center" }}>{message}</span>
       </div>
     </>
   );
