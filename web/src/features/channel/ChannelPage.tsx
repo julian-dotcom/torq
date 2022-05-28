8b09ebfcdb9c4dc6fc86917f46bf839f4d89ee12
Ole Henrik Skogstrøm <oleskogstrom@gmail.com>
2022-05-28 12:58:06 +0200

    Improve styling of channel event list

--- web/src/features/channel/ChannelPage.tsx
+++ web/src/features/channel/ChannelPage.tsx
@@ -18,10 +18,35 @@ import { useParams } from "react-router";
 import { selectFlowKeys, updateFlowKey } from "./channelSlice";
 import eventIcons from "../charts/plots/eventIcons";
 
-// TODO: Add lookup table for better labels in event list
-// const eventLabels = {
-//
-// }
+const f = d3.format(",.2s");
+function fm(value: number): string | number {
+  if (value > 1) {
+    return f(value);
+  }
+  return value;
+}
+
+function formatEventText(type: string, value: number, prev: number, outbound: boolean): string {
+  const changed = value > prev ? "increased" : "decreased";
+  const changeText = `${changed} from ${fm(prev)} to ${fm(value)}`;
+  switch (type) {
+    case "fee_rate":
+      return `Fee rate ${outbound ? "outbound" : "inbound"} ${changeText}`;
+    case "base_fee":
+      return `Base fee ${outbound ? "outbound" : "inbound"} ${changeText}`;
+    case "min_htlc":
+      return `Min HTLC ${outbound ? "outbound" : "inbound"} ${changeText}`;
+    case "max_htlc":
+      return `Max HTLC ${outbound ? "outbound" : "inbound"} ${changeText}`;
+    case "rebalanced":
+      return `Rebalanced ${f(value)} ${outbound ? "outbound" : "inbound"}`;
+    case "disabled":
+      return `Disabled channel ${outbound ? "outbound" : "inbound"}`;
+    case "enabled":
+      return `Enabled channel ${outbound ? "outbound" : "inbound"}`;
+  }
+  return "";
+}
 
 function ChannelPage() {
   const currentPeriod = useAppSelector(selectTimeInterval);
@@ -41,6 +66,8 @@ function ChannelPage() {
   });
 
   const flowKey = useAppSelector(selectFlowKeys);
+  let prev: string;
+  let prevAlias: string;
 
   return (
     <div className={styles.channelsPageContent}>
@@ -152,20 +179,52 @@ function ChannelPage() {
           </div>
         </div>
 
-        <div className={classNames(styles.pageRow, styles.eventSummary)} style={{ height: "600px" }}>
+        <div className={classNames(styles.pageRow, styles.eventSummary)}>
           <div className={styles.shortColumn}>
             <div className={classNames(styles.card, styles.scroll)} style={{ height: "600px" }}>
               <div className={styles.eventRowsWrapper}>
                 {historyQuery?.data?.events &&
                   historyQuery.data.events.map((event: any, index: number) => {
                     const icon = eventIcons.get(event.type);
+                    const newDate = prev !== event.date;
+                    const newAlias = prevAlias !== event.channel_point;
+                    prev = event.date;
+                    prevAlias = event.channel_point;
+                    const chan =
+                      (historyQuery?.data?.channels || []).find((c: any) => c.channel_point === event.channel_point) ||
+                      {};
+                    console.log(chan);
                     return (
-                      <div key={index} className={styles.eventRow}>
-                        <div className={styles.datetime}>{format(new Date(event.datetime), "yyyy-MM-dd hh:mm")}</div>
-                        <div className={"event-type"} dangerouslySetInnerHTML={{ __html: icon as string }} />
-                        <div className={"event-previousValue"}>{d3.format(",.2s")(event.previous_value)}</div>
-                        <div className={"event-value"}>{d3.format(",.2s")(event.value)}</div>
-                      </div>
+                      <>
+                        {newDate && (
+                          <div key={"date-row" + index} className={styles.eventDateRow}>
+                            {format(new Date(event.date), "yyyy-MM-dd")}
+                          </div>
+                        )}
+                        {(newDate || newAlias) && (
+                          <div className={styles.eventRowName}>
+                            <div className={styles.channelAlias}>{chan.alias}</div>
+                            <div>|</div>
+                            <div className={styles.channelPoint}>{chan.channel_point}</div>
+                          </div>
+                        )}
+                        <div
+                          key={index}
+                          className={classNames(
+                            styles.eventRow,
+                            styles[event.type],
+                            styles[event.outbound ? "" : "inbound"]
+                          )}
+                        >
+                          <div className={styles.eventRowDetails}>
+                            <div className={styles.datetime}>{format(new Date(event.datetime), "hh:mm")}</div>
+                            <div className={"event-type"} dangerouslySetInnerHTML={{ __html: icon as string }} />
+                            <div className={"event-type-label"}>
+                              {formatEventText(event.type, event.value, event.previous_value, event.outbound)}
+                            </div>
+                          </div>
+                        </div>
+                      </>
                     );
                   })}
               </div>
