508b463a9d473a9d4541a176cc784504844e19dc
Max Edwards <youwontforgetthis@gmail.com>
2022-06-24 18:31:37 +0100

    made sed command on configure portable

--- docker/configure.sh
+++ docker/configure.sh
@@ -22,7 +22,8 @@ printf "\n"
 [ -f docker-compose.yml ] && rm docker-compose.yml
 
 cp example-docker-compose.yml docker-compose.yml
-sed -i '' "s/\<YourDBPassword\>/$DBPASSWORD/" docker-compose.yml
-sed -i '' "s/\<YourUIPassword\>/$UIPASSWORD/" docker-compose.yml
+# https://stackoverflow.com/questions/16745988/sed-command-with-i-option-in-place-editing-works-fine-on-ubuntu-but-not-mac
+sed -i.bak "s/<YourDBPassword>/$DBPASSWORD/" docker-compose.yml && rm docker-compose.yml.bak
+sed -i.bak "s/<YourUIPassword>/$UIPASSWORD/" docker-compose.yml && rm docker-compose.yml.bak
 
 echo 'Docker compose file (docker-compose.yml) created'
