d1db32d46a0d51a4d3d2f6687d57851cb8cfb0ba
Max Edwards <youwontforgetthis@gmail.com>
2022-06-24 11:46:15 +0100

    updated path on reverse proxy example

--- docker/reverse-proxy-example.sh
+++ docker/reverse-proxy-example.sh
@@ -1,1 +1,1 @@
-docker run --name reverseproxy --mount type=bind,source=/Users/maxedwards/source/lncapital/torq/docker/nginx.conf,target=/etc/nginx/nginx.conf,readonly -p 132:132 --rm nginx
+docker run --name reverseproxy --mount type=bind,source=<absolutepath>/nginx.conf,target=/etc/nginx/nginx.conf,readonly -p 132:132 --rm nginx
