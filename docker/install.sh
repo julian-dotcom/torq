794bf8346ae683afbb35ed4c625464ee1293fb84
Ole Henrik Skogstrom <ohenrik@Oles-MacBook-Pro.local>
2022-07-08 11:52:37 +0200

    Improved install script

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 echo Configuring docker-compose file
 
 printf "\n"
@@ -8,10 +9,9 @@ stty echo
 printf "Directory (default: ~/.torq):"
 read TORQDIR
 stty echo
-TORQDIR="${TORQDIR:=$HOME/.torq}"
-mkdir -p $TORQDIR
-printf "\n"
+eval TORQDIR="${TORQDIR:=$HOME/.torq}"
 echo $TORQDIR
+printf "\n"
 
 printf "\n"
 echo Please set a database password
@@ -30,17 +30,29 @@ stty echo
 printf "\n"
 printf "\n"
 
+mkdir -p $TORQDIR
+
 [ -f docker-compose.yml ] && rm docker-compose.yml
 
-curl --location --silent --output $TORQDIR"/docker-compose.yml" https://raw.githubusercontent.com/lncapital/torq/main/docker/example-docker-compose.yml
-curl --location --silent --output $TORQDIR"/start-torq.sh" https://raw.githubusercontent.com/lncapital/torq/main/docker/start.sh
-curl --location --silent --output $TORQDIR"/stop-torq.sh" https://raw.githubusercontent.com/lncapital/torq/main/docker/stop.sh
-curl --location --silent --output $TORQDIR"/update-torq.sh" https://raw.githubusercontent.com/lncapital/torq/main/docker/update.sh
-curl --location --silent --output $TORQDIR"/delete-torq.sh" https://raw.githubusercontent.com/lncapital/torq/main/docker/delete.sh
+START_COMMAND='start-torq'
+STOP_COMMAND='stop-torq'
+UPDATE_COMMAND='update-torq'
+DELETE_COMMAND='delete-torq'
+
+curl --location --silent --output "${TORQDIR}/docker-compose.yml" https://raw.githubusercontent.com/lncapital/torq/main/docker/example-docker-compose.yml
+curl --location --silent --output "${TORQDIR}/${START_COMMAND}" https://raw.githubusercontent.com/lncapital/torq/main/docker/start.sh
+curl --location --silent --output "${TORQDIR}/${STOP_COMMAND}" https://raw.githubusercontent.com/lncapital/torq/main/docker/stop.sh
+curl --location --silent --output "${TORQDIR}/${UPDATE_COMMAND}" https://raw.githubusercontent.com/lncapital/torq/main/docker/update.sh
+curl --location --silent --output "${TORQDIR}/${DELETE_COMMAND}" https://raw.githubusercontent.com/lncapital/torq/main/docker/delete.sh
+
+chmod +x $TORQDIR/$START_COMMAND
+chmod +x $TORQDIR/$STOP_COMMAND
+chmod +x $TORQDIR/$UPDATE_COMMAND
+chmod +x $TORQDIR/$DELETE_COMMAND
 
 # https://stackoverflow.com/questions/16745988/sed-command-with-i-option-in-place-editing-works-fine-on-ubuntu-but-not-mac
-sed -i.bak "s/<YourDBPassword>/$DBPASSWORD/"  $TORQDIR"/docker-compose.yml" && rm $TORQDIR"/docker-compose.yml.bak"
-sed -i.bak "s/<YourUIPassword>/$UIPASSWORD/" $TORQDIR"/docker-compose.yml" && rm $TORQDIR"/docker-compose.yml.bak"
+sed -i.bak "s/<YourDBPassword>/$DBPASSWORD/" $TORQDIR/docker-compose.yml && rm $TORQDIR/docker-compose.yml.bak
+sed -i.bak "s/<YourUIPassword>/$UIPASSWORD/" $TORQDIR/docker-compose.yml && rm $TORQDIR/docker-compose.yml.bak
 
 echo 'Docker compose file (docker-compose.yml) created in '$TORQDIR
 
@@ -48,16 +60,18 @@ Green='\033[0;32m' # Green text color
 Cyan='\033[0;36m'
 Red='\033[0;31m'
 NC='\033[0m' ## Reset text color
-START_COMMAND='start-torq.sh'
 
-echo 'We have added these scripts to 'TORQDIR':\n'
+
+echo "We have added these scripts to ${TORQDIR}:\n"
 echo "${Cyan}${START_COMMAND}${NC}\t (This command starts Torq)"
-echo "${Cyan}stop-torq.sh${NC}\t (This command stops Torq)"
-echo "${Cyan}update-torq.sh${NC}\t (This command updates Torq)"
-echo "${Red}delete-torq.sh${NC}\t (WARNING: This command deletes Torq _including_ all collected data!)"
+echo "${Cyan}${STOP_COMMAND}${NC}\t (This command stops Torq)"
+echo "${Cyan}${UPDATE_COMMAND}${NC}\t (This command updates Torq)"
+echo "${Red}${DELETE_COMMAND}${NC}\t (WARNING: This command deletes Torq _including_ all collected data!)"
 
-echo "Optional: You can add these commands to your path"
+
+echo "${Green}Optional:${NC} you can add these scripts to your PATH by running:"
+echo "sudo ln -s ${TORQDIR}/* /usr/local/bin/"
 
 echo "\nTry it out! Start Torq now with:"
-echo "${Green}sh ${TORQDIR}/${START_COMMAND}${NC}"
+echo "${Green}${TORQDIR}/${START_COMMAND}${NC}"
 echo "\n"
