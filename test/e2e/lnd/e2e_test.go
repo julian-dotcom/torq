e041e50bb212aa0966cd2aa0e97a207afb9bb08f
Max Edwards <youwontforgetthis@gmail.com>
2022-07-18 14:31:53 +0100

    added video archive on github actions

--- test/e2e/lnd/e2e_test.go
+++ test/e2e/lnd/e2e_test.go
@@ -912,3 +912,46 @@ func TestPayCarolFromAlice(t *testing.T) {
 	log.Println("Payment received by Carol")
 	log.Printf("Carol's channel balance: %s\n", carolChannelBalance)
 }
+
+func TestPlaywrightVideo(t *testing.T) {
+	pw, err := playwright.Run()
+	if err != nil {
+		log.Fatalf("could not launch playwright: %v", err)
+	}
+	browser, err := pw.Chromium.Launch()
+	if err != nil {
+		log.Fatalf("could not launch Chromium: %v", err)
+	}
+	page, err := browser.NewPage(playwright.BrowserNewContextOptions{
+		RecordVideo: &playwright.BrowserNewContextOptionsRecordVideo{
+			Dir: playwright.String("e2e_videos/"),
+		},
+	})
+	if err != nil {
+		log.Fatalf("could not create page: %v", err)
+	}
+	gotoPage := func(url string) {
+		fmt.Printf("Visiting %s\n", url)
+		if _, err = page.Goto(url); err != nil {
+			log.Fatalf("could not goto: %v", err)
+		}
+		fmt.Printf("Visited %s\n", url)
+	}
+	gotoPage("http://whatsmyuseragent.org")
+	gotoPage("https://github.com")
+	gotoPage("https://microsoft.com")
+	if err := page.Close(); err != nil {
+		log.Fatalf("failed to close page: %v", err)
+	}
+	path, err := page.Video().Path()
+	if err != nil {
+		log.Fatalf("failed to get video path: %v", err)
+	}
+	fmt.Printf("Saved to %s\n", path)
+	if err = browser.Close(); err != nil {
+		log.Fatalf("could not close browser: %v", err)
+	}
+	if err = pw.Stop(); err != nil {
+		log.Fatalf("could not stop Playwright: %v", err)
+	}
+}
