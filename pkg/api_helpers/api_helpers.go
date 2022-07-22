f0dc540522f6c2593a2121b2fc0dd7797de78d30
ohenrik <oleskogstrom@gmail.com>
2022-07-22 14:26:01 +0200

    Correct json names of general api response

--- pkg/api_helpers/api_helpers.go
+++ pkg/api_helpers/api_helpers.go
@@ -2,11 +2,11 @@ package api_helpers
 
 type Pagination struct {
 	Total  uint64 `json:"total"`
-	Limit  uint64 `json:"Limit"`
-	Offset uint64 `json:"Offset"`
+	Limit  uint64 `json:"limit"`
+	Offset uint64 `json:"offset"`
 }
 
 type ApiResponse struct {
-	Data       interface{}
-	Pagination Pagination
+	Data       interface{} `json:"data"`
+	Pagination Pagination  `json:"pagination"`
 }
