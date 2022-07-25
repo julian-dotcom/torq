b6071e13a0277b0359b06890dacc1c058dc0e526
ohenrik <oleskogstrom@gmail.com>
2022-07-25 17:08:15 +0200

    Fix payments in flight test

--- pkg/lnd/payments.go
+++ pkg/lnd/payments.go
@@ -236,7 +236,8 @@ func fetchInFlightPaymentIndexes(db *sqlx.DB) (r []uint64, err error) {
 	rows, err := db.Query(`
 		select payment_index
 		from payment
-		where status = 'IN_FLIGHT';
+		where status = 'IN_FLIGHT'
+		order by payment_index asc;
 	`)
 	if err != nil {
 		return nil, err
