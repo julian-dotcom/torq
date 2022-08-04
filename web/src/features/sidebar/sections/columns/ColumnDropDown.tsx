44af904c61bbf21607f1187aa3f24791f96c4ad9
ohenrik <oleskogstrom@gmail.com>
2022-08-04 13:16:54 +0200

    Updated colors for columns sidebar section

--- web/src/features/sidebar/sections/columns/ColumnDropDown.tsx
+++ web/src/features/sidebar/sections/columns/ColumnDropDown.tsx
@@ -22,7 +22,7 @@ const customStyles = {
   }),
   dropdownIndicator: (provided: any, state: any) => ({
     ...provided,
-    color: "var(--secondary-1-500)",
+    color: "var(--secondary-2-500)",
     // padding: "0",
   }),
   control: (provided: any, state: any) => ({
@@ -34,7 +34,7 @@ const customStyles = {
     minHeight: "34px",
     "&:hover": {
       border: "1px solid transparent",
-      // backgroundColor: "var(--secondary-1-100)",
+      // backgroundColor: "var(--secondary-2-100)",
       boxShadow: "none",
       borderRadius: "0px",
     },
@@ -46,22 +46,22 @@ const customStyles = {
   option: (provided: any, state: any) => ({
     ...provided,
     color: "var(--content-default)",
-    // background: state.isFocused ? "var(--secondary-1-50)" : "#ffffff",
-    background: state.isSelected ? "var(--secondary-1-100)" : "#ffffff",
+    // background: state.isFocused ? "var(--secondary-2-50)" : "#ffffff",
+    background: state.isSelected ? "var(--secondary-2-100)" : "#ffffff",
     // background: state.isOptionSelected ? "var(--primary-150)" : "#ffffff",
     "&:hover": {
       boxShadow: "none",
-      backgroundColor: "var(--secondary-1-50)",
+      backgroundColor: "var(--secondary-2-50)",
     },
     // fontSize: "var(--font-size-small)",
   }),
   menuList: (provided: any, state: any) => ({
     ...provided,
     // background: "",
-    border: "1px solid var(--secondary-1-150)",
+    border: "1px solid var(--secondary-2-150)",
     boxShadow: "none",
     borderRadius: "2px",
-    // background: "var(--secondary-1-500)",
+    // background: "var(--secondary-2-500)",
   }),
   menu: (provided: any, state: any) => ({
     ...provided,
@@ -69,10 +69,10 @@ const customStyles = {
   }),
   container: (provided: any, state: any) => ({
     ...provided,
-    border: "1px solid var(--secondary-1-100)",
+    border: "1px solid var(--secondary-2-100)",
     "&:hover": {
-      border: "1px solid var(--secondary-1-150)",
-      // backgroundColor: "var(--secondary-1-100)",
+      border: "1px solid var(--secondary-2-150)",
+      // backgroundColor: "var(--secondary-2-100)",
       boxShadow: "none",
       borderRadius: "2px",
     },
