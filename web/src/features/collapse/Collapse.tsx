15c8d10ba1b68f65419dbf6965d89454181d5424
Max Edwards <youwontforgetthis@gmail.com>
2022-08-09 18:39:40 +0100

    added collapse component

---
+++ web/src/features/collapse/Collapse.tsx
@@ -0,0 +1,15 @@
+import { ReactElement, useLayoutEffect, useRef, useState } from "react";
+export default (props: { children: ReactElement; collapsed: boolean }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [styleState, setStyleState] = useState({ height: "auto", transition: "0.5s" });
+  useLayoutEffect(() => {
+    if (ref.current) {
+      setStyleState({ ...styleState, height: props.collapsed ? "0" : ref.current.scrollHeight + "px" });
+    }
+  }, [props]);
+  return (
+    <div ref={ref} style={styleState}>
+      {props.children}
+    </div>
+  );
+};
