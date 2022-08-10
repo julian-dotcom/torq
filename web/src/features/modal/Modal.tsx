2993ccc26209191ea0addc1aff3e6e7c3fb7724c
Max Edwards <youwontforgetthis@gmail.com>
2022-08-10 16:43:55 +0100

    added modal component

---
+++ web/src/features/modal/Modal.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./Modal.module.scss";
+import { DismissCircle20Regular as DismissIcon } from "@fluentui/react-icons";
+import classNames from "classnames";
+
+interface ModalProps {
+  children: React.ReactNode;
+  show: boolean;
+  title?: string;
+  icon?: React.ReactNode;
+  onClose: Function;
+}
+
+const Modal = (props: ModalProps) => {
+  const handleClose = () => {
+    props.onClose();
+  };
+  const ignore = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+  return (
+    <div className={classNames(styles.modal, { [styles.show]: props.show })} onClick={handleClose}>
+      <div className={styles.content} onClick={ignore}>
+        <div className={styles.header}>
+          {props.icon && <span className={styles.icon}>{props.icon}</span>}
+          <span className={styles.title}>{props.title}</span>
+          <span className={styles.close} onClick={handleClose}>
+            <DismissIcon />
+          </span>
+        </div>
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
