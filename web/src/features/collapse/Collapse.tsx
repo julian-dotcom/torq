cb6e54c0a30af5fb23b9f988dbf361b9008e8b5e
Max Edwards <youwontforgetthis@gmail.com>
2022-08-18 18:07:37 +0100

    collapse is snappier and can now be switched off

--- web/src/features/collapse/Collapse.tsx
+++ web/src/features/collapse/Collapse.tsx
@@ -1,7 +1,11 @@
 import { ReactElement, useLayoutEffect, useRef, useState } from "react";
-export default (props: { children: ReactElement; collapsed: boolean }) => {
+export default (props: { children: ReactElement; collapsed: boolean; animate: boolean }) => {
   const ref = useRef<HTMLDivElement>(null);
-  const [styleState, setStyleState] = useState({ height: "auto", transition: "0.5s", overflow: "hidden" });
+  const [styleState, setStyleState] = useState({
+    height: "auto",
+    overflow: "hidden",
+    transition: props.animate ? "height 0.3s" : "none",
+  });
   useLayoutEffect(() => {
     if (ref.current) {
       setStyleState({ ...styleState, height: props.collapsed ? "0" : ref.current.scrollHeight + "px" });
