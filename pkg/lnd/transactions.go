92bb45704ebaeb8d320e5efc21c39ba101a09b87
Max Edwards <youwontforgetthis@gmail.com>
2022-09-01 18:33:48 +0100

    updated last tx height sql check to coalesce

--- pkg/lnd/transactions.go
+++ pkg/lnd/transactions.go
@@ -2,7 +2,6 @@ package lnd
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"github.com/cockroachdb/errors"
 	"github.com/jmoiron/sqlx"
@@ -15,14 +14,10 @@ import (
 
 func fetchLastTxHeight(db *sqlx.DB) (txHeight int32, err error) {
 
-	sqlLatest := `select max(block_height) from tx;`
+	sqlLatest := `select coalesce(max(block_height),1) from tx;`
 
 	row := db.QueryRow(sqlLatest)
 	err = row.Scan(&txHeight)
-
-	if err == sql.ErrNoRows {
-		return 1, nil
-	}
 
 	if err != nil {
 		return 1, err
