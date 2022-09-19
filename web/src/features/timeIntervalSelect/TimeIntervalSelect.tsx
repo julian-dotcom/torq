a511b608316017dea75efcea95e1bca162624344
ohenrik <oleskogstrom@gmail.com>
2022-09-19 16:41:37 +0200

    Cleaned up styling issues

--- web/src/features/timeIntervalSelect/TimeIntervalSelect.tsx
+++ web/src/features/timeIntervalSelect/TimeIntervalSelect.tsx
@@ -1,3 +1,6 @@
+// DateRange Picker
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 import "./interval_select.scss";
 import React, { useState } from "react";
 import { format, startOfDay, addDays, subDays, differenceInDays } from "date-fns";
@@ -44,7 +47,7 @@ function TimeIntervalSelect(props: { className?: string }) {
   };
 
   const renderCustomRangeLabel = () => (
-    <div onClick={() => setIsMobile(true)} className="">
+    <div onClick={() => setIsMobile(true)} className="custom-mobile">
       Custom
     </div>
   );
