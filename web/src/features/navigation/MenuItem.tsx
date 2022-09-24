6255a8b89d448504aeb11de5898cce54084312b0
Guilherme Salomão <cryptosalomao@gmail.com>
2022-09-24 07:18:17 -0300

    fix: highlight current navigation item only

--- web/src/features/navigation/MenuItem.tsx
+++ web/src/features/navigation/MenuItem.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import styles from "./nav.module.scss";
 import classNames from "classnames";
-import { NavLink } from "react-router-dom";
+import { NavLink, useMatch, useResolvedPath } from "react-router-dom";
 
 function MenuItem(props: { text: string; icon?: any; routeTo: string }) {
-  const linkClasses = (p: { isActive: boolean }): string => {
-    return classNames(styles.title, { [styles.selected]: p.isActive });
-  };
+  const resolvedPath = useResolvedPath(props.routeTo);
+  const hasMatch = useMatch({ path: resolvedPath.pathname, end: true });
+
+  const resolvedClassNames = classNames(styles.title, { [styles.selected] : hasMatch });
 
   return (
     <div className={classNames(styles.item)}>
       <div className={classNames(styles.contentWrapper)}>
-        <NavLink to={props.routeTo} className={linkClasses}>
+        <NavLink to={props.routeTo} className={resolvedClassNames}>
           <div className={styles.icon}>{props.icon}</div>
           <div className={classNames(styles.text)}>{props.text}</div>
         </NavLink>
