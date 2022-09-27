4644f69db1e8f295a10006e617e4c286e6b8237a
Max Edwards <youwontforgetthis@gmail.com>
2022-09-27 18:06:49 +0100

    bumped to v0.8.6

--- build/version.go
+++ build/version.go
@@ -24,7 +24,7 @@ const (
 	AppMinor uint = 8
 
 	// AppPatch defines the application patch for this binary.
-	AppPatch uint = 5
+	AppPatch uint = 6
 
 	// AppPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
