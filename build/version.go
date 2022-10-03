5a81d962ac0955bfa484abb849f692591cf2a37f
Max Edwards <youwontforgetthis@gmail.com>
2022-10-03 11:34:12 +0100

    bumped version to 0.8.9

--- build/version.go
+++ build/version.go
@@ -24,7 +24,7 @@ const (
 	AppMinor uint = 8
 
 	// AppPatch defines the application patch for this binary.
-	AppPatch uint = 8
+	AppPatch uint = 9
 
 	// AppPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
