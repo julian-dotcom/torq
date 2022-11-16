a7f7606e254f6fe7b4843750c4f0480b970f799c
ohenrik <oleskogstrom@gmail.com>
2022-11-16 15:35:00 +0100

    Fixed decode invoice request order bug

--- internal/invoices/routes.go
+++ internal/invoices/routes.go
@@ -5,10 +5,9 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-
 func RegisterInvoicesRoutes(r *gin.RouterGroup, db *sqlx.DB) {
 	r.GET("", func(c *gin.Context) { getInvoicesHandler(c, db) })
-	r.GET("decode/", func(c *gin.Context) { decodeInvoiceHandler(c, db) })
+	r.GET("decode", func(c *gin.Context) { decodeInvoiceHandler(c, db) })
 	r.GET(":identifier", func(c *gin.Context) { getInvoiceHandler(c, db) })
 	r.POST("newinvoice", func(c *gin.Context) { newInvoiceHandler(c, db) })
 }
