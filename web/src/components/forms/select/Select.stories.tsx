20174a137cebf4145ebefbd5b2e1f54ab5bd125c
ohenrik <oleskogstrom@gmail.com>
2022-11-16 14:28:04 +0100

    Cleaned up and moved select input

---
+++ web/src/components/forms/select/Select.stories.tsx
@@ -0,0 +1,54 @@
+import { Meta, Story } from "@storybook/react";
+import Select, { SelectProps } from "./Select";
+import { InputColorVaraint } from "components/forms/select/variants";
+import { InputSizeVariant } from "./variants";
+// import { useArgs } from "@storybook/client-api";
+
+export default {
+  title: "components/forms/Select",
+  component: Select,
+} as Meta;
+
+const Template: Story<SelectProps> = (args) => {
+  // const [_, updateArgs] = useArgs();
+  return (
+    <div style={{ height: "400px" }}>
+      <Select {...args} />
+    </div>
+  );
+};
+
+const defaultArgs: SelectProps = {
+  label: "Label",
+  colorVariant: InputColorVaraint.primary,
+  sizeVariant: InputSizeVariant.normal,
+  options: [
+    { value: "1", label: "Option 1" },
+    { value: "2", label: "Option 2" },
+    { value: "3", label: "Option 3" },
+  ],
+};
+
+export const Primary = Template.bind({});
+Primary.args = defaultArgs;
+
+export const Open = Template.bind({});
+Open.args = {
+  ...defaultArgs,
+  menuIsOpen: true,
+};
+
+export const Accent1 = Template.bind({});
+Accent1.args = { ...defaultArgs, colorVariant: InputColorVaraint.accent1 };
+
+export const Accent2 = Template.bind({});
+Accent2.args = { ...defaultArgs, colorVariant: InputColorVaraint.accent2 };
+
+export const Accent3 = Template.bind({});
+Accent3.args = { ...defaultArgs, colorVariant: InputColorVaraint.accent3 };
+
+export const Warning = Template.bind({});
+Warning.args = { ...defaultArgs, warningText: "Warning: Something needs attention" };
+
+export const Error = Template.bind({});
+Error.args = { ...defaultArgs, errorText: "Error: Something is wrong" };
---
+++ web/src/components/forms/select/Select.tsx
@@ -0,0 +1,133 @@
+import Select, { Props, components } from "react-select";
+import {
+  ChevronDown16Regular as ChevronDownIcon,
+  WarningRegular as WarningIcon,
+  ErrorCircleRegular as ErrorIcon,
+} from "@fluentui/react-icons";
+import styles from "./select.module.scss";
+import classNames from "classnames";
+import { GetColorClass, GetSizeClass, InputColorVaraint, InputSizeVariant } from "./variants";
+import { useId } from "react";
+
+export type SelectOptionType = { value: string; label: string };
+
+const customStyles = {
+  indicatorSeparator: () => {
+    return {};
+  },
+  control: (provided: any, state: any) => ({
+    ...provided,
+    borderRadius: 4,
+    backgroundColor: state.isFocused ? "var(--input-focus-background)" : "var(--input-default-background)",
+    borderColor: state.isFocused ? "var(--input-focus-border-color)" : "transparent",
+    boxShadow: "none",
+    "&:hover": {
+      backgroundColor: state.isFocused ? "var(--input-focus-background)" : "var(--input-hover-background)",
+      boxShadow: "none",
+    },
+    height: "40px",
+  }),
+  placeholder: (provided: any) => {
+    return {
+      ...provided,
+      color: "var(--input-placeholder-color)",
+    };
+  },
+  dropdownIndicator: (provided: any, _: any) => ({
+    ...provided,
+    color: "var(--input-color)",
+    // padding: "0",
+  }),
+  singleValue: (provided: any) => ({
+    ...provided,
+    color: "var(--input-color)",
+  }),
+  option: (provided: any, state: any) => ({
+    ...provided,
+    color: "var(--input-color)",
+    background: state.isFocused ? "var(--input-default-background)" : "#ffffff",
+    "&:hover": {
+      boxShadow: "none",
+      backgroundColor: "var(--input-hover-background)",
+    },
+    borderRadius: "4px",
+  }),
+  menuList: (provided: any, _: any) => ({
+    ...provided,
+    borderColor: "transparent",
+    boxShadow: "none",
+    padding: "8px",
+  }),
+  menu: (provided: any, _: any) => ({
+    ...provided,
+    borderColor: "transparent",
+    borderRadius: "4px",
+    boxShadow: "var(--hover-box-shadow)",
+    zIndex: "10",
+  }),
+};
+
+export type SelectProps = Props & {
+  label?: string;
+  colorVariant?: InputColorVaraint;
+  sizeVariant?: InputSizeVariant;
+  warningText?: string;
+  errorText?: string;
+};
+
+export default function TorqSelect({
+  label,
+  colorVariant,
+  sizeVariant,
+  warningText,
+  errorText,
+  ...selectProps
+}: SelectProps) {
+  const DropdownIndicator = (props: any) => {
+    return (
+      <components.DropdownIndicator {...props}>
+        <ChevronDownIcon />
+      </components.DropdownIndicator>
+    );
+  };
+  const inputId = useId();
+  let inputColorClass = GetColorClass(colorVariant);
+  if (warningText != undefined) {
+    inputColorClass = GetColorClass(InputColorVaraint.warning);
+  }
+  if (errorText != undefined) {
+    inputColorClass = GetColorClass(InputColorVaraint.error);
+  }
+  return (
+    <div className={classNames(styles.inputWrapper, GetSizeClass(sizeVariant), inputColorClass)}>
+      <div className={styles.labelWrapper}>
+        <label htmlFor={selectProps.id || inputId} className={styles.label}>
+          {label}
+        </label>
+      </div>
+      <Select
+        id={selectProps.id || inputId}
+        components={{ DropdownIndicator }}
+        className={selectProps.className}
+        styles={customStyles}
+        {...selectProps}
+      />
+      {errorText && (
+        <div className={classNames(styles.feedbackWrapper, styles.feedbackError)}>
+          <div className={styles.feedbackIcon}>
+            <ErrorIcon />
+          </div>
+          <div className={styles.feedbackText}>{errorText}</div>
+        </div>
+      )}
+      {warningText && (
+        <div className={classNames(styles.feedbackWrapper, styles.feedbackWarning)}>
+          <div className={styles.feedbackIcon}>
+            <WarningIcon />
+          </div>
+          <div className={styles.feedbackText}>{warningText}</div>
+        </div>
+      )}
+    </div>
+  );
+}
