735e8dfcf80c9710433a417a9192fa0a013c571d
Max Edwards <youwontforgetthis@gmail.com>
2022-11-25 09:56:44 +0000

    added fee policy routes

---
+++ internal/fee_policy/routes.go
@@ -0,0 +1,19 @@
+package fee_policy
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+)
+
+func RegisterChannelRoutes(r *gin.RouterGroup, db *sqlx.DB) {
+	r.POST("", func(c *gin.Context) { addFeePolicyHandler(c, db) })
+	r.GET("", func(c *gin.Context) { getFeePolicyListHandler(c, db) })
+}
+
+func addFeePolicyHandler(c *gin.Context, db *sqlx.DB) {
+	return
+}
+
+func getFeePolicyListHandler(c *gin.Context, db *sqlx.DB) {
+	return
+}
