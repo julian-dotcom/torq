4120e837d9008aadb1989e4fc11e7a4cb2579ef2
Max Edwards <youwontforgetthis@gmail.com>
2022-12-13 19:35:51 +0000

    added network slice to keep track of the active network

--- web/src/features/navigation/QuickToggles.tsx
+++ web/src/features/navigation/QuickToggles.tsx
@@ -1,13 +1,14 @@
-/* import { useAppDispatch } from "store/hooks"; */
-/* import { toggleNav } from "./navSlice"; */
-/* import classNames from "classnames"; */
+import { useAppDispatch, useAppSelector } from "store/hooks";
 import { Globe20Regular as GlobeIcon } from "@fluentui/react-icons";
 import styles from "./nav.module.scss";
 import Popover from "features/popover/Popover";
 import Button, { buttonColor, buttonSize } from "components/buttons/Button";
+import { selectActiveNetwork, setActiveNetwork, Network } from "features/network/networkSlice";
 
 function QuickToggles() {
-  /* const dispatch = useAppDispatch(); */
+  const dispatch = useAppDispatch();
+  const activeNetwork = useAppSelector(selectActiveNetwork);
+
   return (
     <div className={styles.quickToggles}>
       <div>
@@ -25,10 +26,26 @@ function QuickToggles() {
           className={"right"}
         >
           <div className={styles.quickToggleContent}>
-            <Button buttonColor={buttonColor.subtle} text="Mainnet"></Button>
-            <Button buttonColor={buttonColor.subtle} text="Testnet"></Button>
-            <Button buttonColor={buttonColor.primary} text="Regtest"></Button>
-            <Button buttonColor={buttonColor.subtle} text="Simnet"></Button>
+            <Button
+              buttonColor={activeNetwork === Network.Mainnet ? buttonColor.primary : buttonColor.subtle}
+              text="Mainnet"
+              onClick={() => dispatch(setActiveNetwork(Network.Mainnet))}
+            ></Button>
+            <Button
+              buttonColor={activeNetwork === Network.Testnet ? buttonColor.primary : buttonColor.subtle}
+              text="Testnet"
+              onClick={() => dispatch(setActiveNetwork(Network.Testnet))}
+            ></Button>
+            <Button
+              buttonColor={activeNetwork === Network.Regtest ? buttonColor.primary : buttonColor.subtle}
+              text="Regtest"
+              onClick={() => dispatch(setActiveNetwork(Network.Regtest))}
+            ></Button>
+            <Button
+              buttonColor={activeNetwork === Network.Simnet ? buttonColor.primary : buttonColor.subtle}
+              text="Simnet"
+              onClick={() => dispatch(setActiveNetwork(Network.Simnet))}
+            ></Button>
           </div>
         </Popover>
       </div>
---
+++ web/src/features/network/networkSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "store/store";
+
+export enum Network {
+  Mainnet,
+  Testnet,
+  Regtest,
+  Simnet,
+}
+
+export interface NetworkState {
+  activeNetwork: Network;
+}
+
+const initialState: NetworkState = {
+  activeNetwork: Network.Mainnet,
+};
+
+export const networkSlice = createSlice({
+  name: "network",
+  initialState,
+
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    setActiveNetwork: (state, action) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.activeNetwork = action.payload;
+    },
+  },
+});
+
+export const { setActiveNetwork } = networkSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+export const selectActiveNetwork = (state: RootState) => state.network.activeNetwork;
+
+export default networkSlice.reducer;
