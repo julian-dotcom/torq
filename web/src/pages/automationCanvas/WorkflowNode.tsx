cfc6ab32764d87d36b972a9ffccbad0cf4247ca4
ohenrik <oleskogstrom@gmail.com>
2022-12-15 03:11:21 +0100

    Working drag&drop cards + canvas

---
+++ web/src/pages/automationCanvas/WorkflowNode.tsx
@@ -0,0 +1,71 @@
+import useTranslations from "services/i18n/useTranslations";
+import styles from "./workflow_page.module.scss";
+import { MutableRefObject, useRef, useState } from "react";
+import classNames from "classnames";
+
+type WorkflowNodeProps = {
+  heading?: string;
+  canvasRef: MutableRefObject<HTMLDivElement>;
+  blankImageRef: MutableRefObject<HTMLCanvasElement>;
+  children?: React.ReactNode;
+};
+
+function WorkflowNode<T>(props: WorkflowNodeProps) {
+  const { t } = useTranslations();
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const itemRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const [nodeBB, setNodeBB] = useState({ left: 0, top: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+
+  // Add a blank canvas to the DOM in order to hide the html5 drag and drop image functionality.
+
+  function handleDragStart(e: React.DragEvent<HTMLDivElement>) {
+    const nodeBB = itemRef.current.getBoundingClientRect();
+    e.dataTransfer.effectAllowed = "move";
+    const x = e.clientX - nodeBB.left;
+    const y = e.clientY - nodeBB.top;
+    e.dataTransfer.setDragImage(props.blankImageRef.current, x, y);
+    setIsDragging(true);
+    setNodeBB({ left: x, top: y });
+  }
+
+  function handleDrag(e: React.DragEvent<HTMLDivElement>) {
+    e.preventDefault();
+    e.stopPropagation();
+    const bb = props.canvasRef.current.getBoundingClientRect();
+    if (e.clientX !== 0 && e.clientY !== 0) {
+      const newX = e.clientX - bb.x - nodeBB.left;
+      const newY = e.clientY - bb.y - nodeBB.top;
+      setPosition({ x: newX, y: newY });
+    }
+  }
+
+  function handleDragEnd(e: React.DragEvent<HTMLDivElement>) {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragging(false);
+  }
+
+  return (
+    <div
+      className={classNames(styles.workflowNodeCard, { [styles.dragging]: isDragging })}
+      style={{ transform: "translate(" + position.x + "px, " + position.y + "px)" }}
+    >
+      <div
+        className={styles.workflowNodeHeader}
+        draggable="true"
+        onDrag={handleDrag}
+        ref={itemRef}
+        onDragEnd={handleDragEnd}
+        onDragStart={handleDragStart}
+        onDragOver={(e) => e.preventDefault()}
+      >
+        {props.heading}
+      </div>
+      <div className={styles.workflowNodeBodyWrapper}>{props.children}</div>
+    </div>
+  );
+}
+
+export default WorkflowNode;
--- web/src/pages/automationCanvas/WorkflowPage.tsx
+++ web/src/pages/automationCanvas/WorkflowPage.tsx
@@ -2,6 +2,7 @@ import useTranslations from "services/i18n/useTranslations";
 import styles from "./workflow_page.module.scss";
 import PageTitle from "features/templates/PageTitle";
 import { MutableRefObject, useRef, useState } from "react";
+import WorkflowNode from "./WorkflowNode";
 import classNames from "classnames";
 
 type WorkflowPageProps = {
@@ -9,101 +10,35 @@ type WorkflowPageProps = {
   breadcrumbs?: Array<any>;
 };
 
-// function useDragging() {
-//   const [isDragging, setIsDragging] = useState(false);
-//   const [pos, setPos] = useState({ x: 0, y: 0 });
-//   const ref = useRef() as MutableRefObject<HTMLDivElement>;
-//   let offsetX = 0;
-//   let offsetY = 0;
-//   console.log(offsetY, offsetX);
-//
-//   function onMouseMove(e: any) {
-//     if (!isDragging) return;
-//     setPos({
-//       x: e.x - offsetX * 2,
-//       y: e.y - offsetY * 2,
-//     });
-//     e.stopPropagation();
-//     e.preventDefault();
-//   }
-//
-//   function onMouseUp(e: any) {
-//     setIsDragging(false);
-//     e.stopPropagation();
-//     e.preventDefault();
-//   }
-//
-//   function onMouseDown(e: any) {
-//     if (e.button !== 0) return;
-//     setIsDragging(true);
-//     // console.log("onMouseDown", e);
-//     // setPos({
-//     //   x: e.x - ref.current.offsetWidth,
-//     //   y: e.y - ref.current.offsetTop,
-//     // });
-//     offsetX = e.x;
-//     offsetY = e.y;
-//
-//     e.stopPropagation();
-//     e.preventDefault();
-//   }
-//
-//   // When the element mounts, attach an mousedown listener
-//   useEffect(() => {
-//     ref.current.addEventListener("mousedown", onMouseDown);
-//
-//     return () => {
-//       ref.current.removeEventListener("mousedown", onMouseDown);
-//     };
-//   }, [ref.current]);
-//
-//   // Everytime the isDragging state changes, assign or remove
-//   // the corresponding mousemove and mouseup handlers
-//   useEffect(() => {
-//     if (isDragging) {
-//       document.addEventListener("mouseup", onMouseUp);
-//       document.addEventListener("mousemove", onMouseMove);
-//     } else {
-//       document.removeEventListener("mouseup", onMouseUp);
-//       document.removeEventListener("mousemove", onMouseMove);
-//     }
-//     return () => {
-//       document.removeEventListener("mouseup", onMouseUp);
-//       document.removeEventListener("mousemove", onMouseMove);
-//     };
-//   }, [isDragging]);
-//
-//   return [ref, pos.x, pos.y, isDragging];
-// }
-
 function WorkflowPage<T>(props: WorkflowPageProps) {
   const { t } = useTranslations();
   const bradcrumbs = props.breadcrumbs || ["Workflow", "1"];
 
   const [position, setPosition] = useState({ x: 0, y: 0 });
-
+  const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
   const canvasRef = useRef() as MutableRefObject<HTMLDivElement>;
-  const itemRef = useRef() as MutableRefObject<HTMLDivElement>;
-  const [itemBB, setItemBB] = useState({ left: 0, top: 0 });
+  const blankImgRef = useRef() as MutableRefObject<HTMLCanvasElement>;
 
+  const [canvasPosition, setCanvasPositionBB] = useState({ left: 0, top: 0 });
   const [isDragging, setIsDragging] = useState(false);
 
   function handleDragStart(e: React.DragEvent<HTMLDivElement>) {
-    const itemBB = itemRef.current.getBoundingClientRect();
-    const x = e.clientX - itemBB.left;
-    const y = e.clientY - itemBB.top;
-    e.dataTransfer.setDragImage(new Image(), x, y);
+    const canvasPosition = canvasRef.current.getBoundingClientRect();
+    e.dataTransfer.effectAllowed = "move";
+    const x = e.clientX - canvasPosition.left;
+    const y = e.clientY - canvasPosition.top;
+    e.dataTransfer.setDragImage(blankImgRef.current, x, y);
     setIsDragging(true);
-    setItemBB({ left: x, top: y });
+    setCanvasPositionBB({ left: x, top: y });
   }
 
   function handleDrag(e: React.DragEvent<HTMLDivElement>) {
     e.preventDefault();
     e.stopPropagation();
-    const bb = canvasRef.current.getBoundingClientRect();
+    const bb = wrapperRef.current.getBoundingClientRect();
     if (e.clientX !== 0 && e.clientY !== 0) {
-      const newX = e.clientX - bb.x - itemBB.left;
-      const newY = e.clientY - bb.y - itemBB.top;
+      const newX = e.clientX - bb.x - canvasPosition.left;
+      const newY = e.clientY - bb.y - canvasPosition.top;
       setPosition({ x: newX, y: newY });
     }
   }
@@ -120,20 +55,24 @@ function WorkflowPage<T>(props: WorkflowPageProps) {
         {props.title}
       </PageTitle>
 
-      <div className={styles.workspaceWrapper}>
-        <div className={styles.workspaceContainer} ref={canvasRef}>
-          <div
-            className={classNames(styles.workflowNodeCard, { [styles.dragging]: isDragging })}
-            draggable="true"
-            onDrag={handleDrag}
-            ref={itemRef}
-            onDragEnd={handleDragEnd}
-            onDragStart={handleDragStart}
-            onDragOver={(e) => e.preventDefault()}
-            style={{ transform: "translate(" + position.x + "px, " + position.y + "px)" }}
-          >
-            dsfasdfasdf
+      <div className={styles.workspaceWrapper} ref={wrapperRef}>
+        <div
+          className={classNames(styles.workspaceCanvas, { [styles.dragging]: isDragging })}
+          draggable="true"
+          onDrag={handleDrag}
+          onDragEnd={handleDragEnd}
+          onDragStart={handleDragStart}
+          onDragOver={(e) => e.preventDefault()}
+        >
+          <div style={{ transform: "translate(" + position.x + "px, " + position.y + "px)" }} ref={canvasRef}>
+            <WorkflowNode canvasRef={canvasRef} heading={"Hello Heading"} blankImageRef={blankImgRef}>
+              {"Hello"}
+            </WorkflowNode>
+            <WorkflowNode canvasRef={canvasRef} heading={"Hello Heading"} blankImageRef={blankImgRef}>
+              {"Hello"}
+            </WorkflowNode>
           </div>
+          <canvas ref={blankImgRef} style={{ width: "1px", height: "1px" }} />
         </div>
       </div>
     </div>
