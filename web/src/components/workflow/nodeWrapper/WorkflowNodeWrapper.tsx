affd84559896e8f8d3c1ef2760d0c251fafb87e6
ohenrik <oleskogstrom@gmail.com>
2022-12-23 00:59:34 +0100

    Switched to existing collapsable component

--- web/src/components/workflow/nodeWrapper/WorkflowNodeWrapper.tsx
+++ web/src/components/workflow/nodeWrapper/WorkflowNodeWrapper.tsx
@@ -5,7 +5,7 @@ import classNames from "classnames";
 import NodeConnector from "./NodeConnector";
 import { CanvasContext } from "components/workflow/canvas/WorkflowCanvas";
 import { ExpandUpRight16Regular as ExpandIcon, ContractDownLeft16Regular as CollapseIcon } from "@fluentui/react-icons";
-// import { ChevronDown16Regular as ExpandIcon, ChevronUp16Regular as CollapseIcon } from "@fluentui/react-icons";
+import Collapse from "features/collapse/Collapse";
 
 type nodeRefType = { nodeRef: MutableRefObject<HTMLDivElement> | null; nodeName: string };
 export const NodeContext = React.createContext<nodeRefType>({
@@ -26,15 +26,12 @@ function WorkflowNodeWrapper<T>(props: WorkflowNodeProps) {
   const { t } = useTranslations();
   const [collapsed, setCollapsed] = useState(true);
   const [position, setPosition] = useState({ x: props.x || 50, y: props.y || 50 });
-  const [bodyStyle, setBodyStyle] = useState({ height: "0px", overflow: "hidden" });
 
   // Canvas and blankRef are used to calculate the position of the node. They are passed down from the canvas
   const { canvasRef, blankImgRef } = useContext(CanvasContext);
 
   // nodeRef is used by the NodeConnector to allow for drag and drop interaction between nodes.
   const nodeRef = createRef() as MutableRefObject<HTMLDivElement>;
-
-  const nodeBodyRef = createRef() as MutableRefObject<HTMLDivElement>;
 
   const headerRef = useRef() as MutableRefObject<HTMLDivElement>;
   const [nodeBB, setNodeBB] = useState({ left: 0, top: 0 });
@@ -81,29 +78,6 @@ function WorkflowNodeWrapper<T>(props: WorkflowNodeProps) {
   function handleCollapse() {
     // Toggle the collapsed state
     setCollapsed(!collapsed);
-
-    // The bounding box of the child element will always be there even if the parrent height is 0px.
-    // This allows us to use the currenty bounding box to set the height of the parrent.
-    const bb = nodeBodyRef.current.getBoundingClientRect();
-
-    if (collapsed) {
-      // Expand the body conetent by setting the body wrapper to the current height of the body.
-      setBodyStyle({ height: bb.height + "px", overflow: "hidden" });
-
-      // Wait before applying the overflow property to avoid content becoming visible too early,
-      // but still allowing dropdowns etc. to show. We also want to set the height to auto, so that
-      // the content can expand to fit the available space.
-      setTimeout(() => {
-        setBodyStyle({ height: "auto", overflow: "visible" });
-      }, 250);
-    } else {
-      // Since we set the height to auto, we need to set it to the current height and wait 1ms before we can transition
-      // to 0px. If not, then the transition will not work and the box size jumps streight to 0.
-      setBodyStyle({ height: bb.height + "px", overflow: "hidden" });
-      setTimeout(() => {
-        setBodyStyle({ height: "0px", overflow: "hidden" });
-      }, 1);
-    }
   }
 
   return (
@@ -135,11 +109,9 @@ function WorkflowNodeWrapper<T>(props: WorkflowNodeProps) {
           {collapsed ? <ExpandIcon /> : <CollapseIcon />}
           <NodeConnector id={connectorId} name={props.nodeName} />
         </div>
-        <div className={classNames(styles.workflowNodeBodyWrapper)} style={bodyStyle}>
-          <div className={styles.workflowNodeBody} ref={nodeBodyRef}>
-            {props.children}
-          </div>
-        </div>
+        <Collapse collapsed={collapsed} animate={true}>
+          <div className={styles.workflowNodeBody}>{props.children}</div>
+        </Collapse>
       </div>
     </NodeContext.Provider>
   );
