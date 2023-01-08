3dd0634423e4a0e3df2bd46da6692fb7ddec8e15
ohenrik <oleskogstrom@gmail.com>
2023-01-08 22:23:01 +0100

    v0.16.0-rc

--- web/src/features/navigation/Navigation.tsx
+++ web/src/features/navigation/Navigation.tsx
@@ -14,7 +14,7 @@ import {
   LockClosed20Regular as LogoutIcon,
   Settings20Regular as SettingsIcon,
   ArrowRouting20Regular as ChannelsIcon,
-  Flash20Regular as WorkflowsIcon,
+  // Flash20Regular as WorkflowsIcon,
   Tag20Regular as TagsIcon,
 } from "@fluentui/react-icons";
 import styles from "./nav.module.scss";
@@ -51,7 +51,7 @@ function Navigation() {
 
         <NavCategory text={t.manage} collapsed={false}>
           <MenuItem text={t.channels} icon={<ChannelsIcon />} routeTo={"/manage/channels"} />
-          <MenuItem text={t.automation} icon={<WorkflowsIcon />} routeTo={"/manage/workflows"} />
+          {/*<MenuItem text={t.automation} icon={<WorkflowsIcon />} routeTo={"/manage/workflows"} />*/}
           <MenuItem text={t.tags} icon={<TagsIcon />} routeTo={"/manage/tags"} />
         </NavCategory>
 
