2a22a12ef09cb1d778ce99aaca81301a28ee68da
Lai <laitang2000@yahoo.com>
2023-02-06 13:44:01 +0000

    Added no cache headers to i18nApi when fetching locale file, the browser will negotiate with the server to see if there is a newer version.

--- web/src/services/i18n/i18nAPI.js
+++ web/src/services/i18n/i18nAPI.js
@@ -1,9 +1,12 @@
 import { langUrl } from "config/i18nConfig";
+const noCacheHeader = new Headers();
+noCacheHeader.append("pragma", "no-cache");
+noCacheHeader.append("cache-control", "no-cache");
 
 export function fetchTranslations(lang) {
   return new Promise((resolve) => {
-    fetch(langUrl.replace("{lang}", lang))
-      .then(response => response.json())
-      .then(data => resolve(data));
+    fetch(langUrl.replace("{lang}", lang), { method: "get", headers: noCacheHeader })
+      .then((response) => response.json())
+      .then((data) => resolve(data));
   });
 }
