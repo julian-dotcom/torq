57a0fb3b2cb8518d3253d46612ccd2f5b67d3b74
Max Edwards <youwontforgetthis@gmail.com>
2023-02-07 19:01:45 +0000

    fixed duplicated server errors

--- web/src/components/errors/errors.ts
+++ web/src/components/errors/errors.ts
@@ -20,7 +20,7 @@ export function mergeServerError(serverErrors: ServerErrorType, existingErrors: 
   if (existingErrors.server === undefined) {
     existingErrors.server = [];
   }
-  existingErrors.server.push(...serverErrors.errors.server);
+  existingErrors.server = serverErrors.errors.server;
   for (const [key, errorCodeOrDescription] of serverErrors.errors.fields) {
     if (!existingErrors.fields.get(key)) {
       existingErrors.fields.set(key, []);
