bb7ac2cde14eb6140077cad735247ce85dcc6bf3
Max Edwards <youwontforgetthis@gmail.com>
2023-02-10 18:01:09 +0000

    removed lodash

--- web/src/features/viewManagement/hooks.ts
+++ web/src/features/viewManagement/hooks.ts
@@ -1,6 +1,6 @@
 import { applyFilters, deserialiseQuery, SerialisableFilterQuery } from "features/sidebar/sections/filter/filter";
 import { OrderBy } from "features/sidebar/sections/sort/SortSection";
-import { orderBy } from "lodash";
+import clone from "clone";
 
 export function useFilterData<T extends Record<string, unknown>>(
   data: Array<T>,
@@ -13,12 +13,22 @@ export function useFilterData<T extends Record<string, unknown>>(
   }
 }
 
-export function useSortData<T>(data: Array<T>, sortBy?: Array<OrderBy>): Array<T> {
-  if (sortBy) {
-    const keys = sortBy.map((s) => s.key);
-    const directions = sortBy.map((s) => s.direction);
-    return orderBy(data, keys, directions);
-  } else {
-    return data;
+export function useSortData<T>(data: Array<T>, orderByList?: Array<OrderBy>): Array<T> {
+  if (orderByList) {
+    data = clone(data); // to allow editing read only data
+    orderByList = clone(orderByList); // to allow reversing the readonly list
+    for (const orderBy of orderByList.reverse()) {
+      data = sortByFn(data, orderBy.key as keyof T, orderBy.direction);
+    }
   }
+  return data;
 }
+
+export const sortByFn = <T, U extends keyof T>(list: T[], index: U, direction: "asc" | "desc"): T[] => {
+  if (!list) return [];
+  return list.sort((a, b) => {
+    if (a[index] < b[index]) return direction === "asc" ? -1 : 1;
+    if (a[index] > b[index]) return direction === "asc" ? 1 : -1;
+    return 0;
+  });
+};
