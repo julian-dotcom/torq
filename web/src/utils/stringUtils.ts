273ebc6233e7be7617f8fc93c2254946a82e7de9
Lai <laitang2000@yahoo.com>
2023-02-24 10:51:55 +0000

    add ignore linting for the enumType there isn't a decent way of passing an enum as a type!

--- web/src/utils/stringUtils.ts
+++ web/src/utils/stringUtils.ts
@@ -1,4 +1,6 @@
-//for a given enum value it attempts to find the key and attempts remove camel casing to display to user
+//for a given enum value it attempts to find the key and attempts remove camel casing to display to user had to use any
+//here because there isn't a good way to pass an enum as an argument!
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function GetFormattedEnumLabelByValue(enumType: any, enumValue: number) {
   const label = Object.keys(enumType).find((key) => enumType[key] === enumValue);
 
