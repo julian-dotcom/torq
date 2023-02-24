60a698320844469cc62288b84dd87f91aa5f3bcb
Max Edwards <youwontforgetthis@gmail.com>
2023-02-24 13:17:45 +0000

    update to error summary to drop useState

--- web/src/components/errors/ErrorSummary.tsx
+++ web/src/components/errors/ErrorSummary.tsx
@@ -2,7 +2,6 @@ import Note, { NoteType } from "features/note/Note";
 import { ErrorCircle20Regular as ErrorIcon } from "@fluentui/react-icons";
 import { FormErrors, replaceMessageMergeTags } from "./errors";
 import useTranslations from "services/i18n/useTranslations";
-import { useState } from "react";
 
 type errorSummaryType = {
   title?: string;
@@ -11,25 +10,25 @@ type errorSummaryType = {
 
 const ErrorSummary = ({ errors, title }: errorSummaryType) => {
   const { t } = useTranslations();
-  const [serverErrorsState, setServerErrorsState] = useState([] as string[]);
+  const serverErrors = [] as string[];
   if (errors && errors.server) {
     for (const error of errors.server) {
       if (error.code) {
         const translatedError = t.errors[error.code];
         const mergedError = replaceMessageMergeTags(translatedError, error.attributes);
-        setServerErrorsState([...serverErrorsState, mergedError]);
+        serverErrors.push(mergedError);
         continue;
       }
       // Bit of a hack to only show the top most error rather than all the wrapped errors
-      setServerErrorsState([...serverErrorsState, (error.description ?? "").split(":")[0]]);
+      serverErrors.push((error.description ?? "").split(":")[0]);
     }
   }
   return (
     <>
       {(errors.server || errors.fields) && (
         <Note icon={<ErrorIcon />} title={title ?? "Error"} noteType={NoteType.error}>
-          {serverErrorsState &&
-            serverErrorsState.map((error, index) => {
+          {serverErrors &&
+            serverErrors.map((error, index) => {
               return <p key={index}>{error}</p>;
             })}
           {errors.fields && <p>See form error{Object.keys(errors.fields).length > 1 && <>s</>}</p>}
