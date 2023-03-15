67c69b4539a19becaa97fc8a9e95e7ecc58b61e6
Max Edwards <youwontforgetthis@gmail.com>
2023-03-15 17:23:45 +0000

    made the inspect click when it's mobile and hover on desktop

--- web/src/components/summary/summaryCard/SummaryCard.tsx
+++ web/src/components/summary/summaryCard/SummaryCard.tsx
@@ -3,6 +3,8 @@ import { Eye20Regular as InspectIcon } from "@fluentui/react-icons";
 import classNames from "classnames";
 import React, { useState } from "react";
 import { format } from "d3";
+import useTouchDevice from "features/touch/useTouchDevice";
+
 export type valueLabel = "" | "btc";
 
 export type SummaryCardProps = {
@@ -16,6 +18,7 @@ export type SummaryCardProps = {
 const formatter = format(",.2f");
 export default function SummaryCard(props: SummaryCardProps) {
   const [showInspection, setShowInspection] = useState<boolean>(false);
+  const { isTouchDevice } = useTouchDevice();
 
   let value = props.value ? props.value : 0;
   let useFormatter = false;
@@ -24,21 +27,32 @@ export default function SummaryCard(props: SummaryCardProps) {
     useFormatter = true;
   }
 
+  const handleHover = (entering: boolean) => {
+    if (!isTouchDevice) {
+      setShowInspection(entering);
+    }
+  };
+
+  const handleClick = () => {
+    if (isTouchDevice) {
+      setShowInspection(!showInspection);
+    }
+  };
+
   return (
     <div
       className={classNames(
-        { [styles.expanded]: showInspection },
+        { [styles.expanded]: showInspection && props.details },
         styles.summaryCard,
         props.summaryClassOverride ? props.summaryClassOverride : ""
       )}
+      onMouseEnter={() => handleHover(true)}
+      onMouseLeave={() => handleHover(false)}
+      onClick={handleClick}
     >
       <div className={styles.headerContainer}>
         <div className={styles.heading}>{props.heading}</div>
-        <div className={classNames(styles.heading, styles.icon)}>
-          {props.details && (
-            <InspectIcon onMouseEnter={() => setShowInspection(true)} onMouseLeave={() => setShowInspection(false)} />
-          )}
-        </div>
+        <div className={classNames(styles.heading, styles.icon)}>{props.details && <InspectIcon />}</div>
       </div>
 
       <div className={styles.valueContainer}>
