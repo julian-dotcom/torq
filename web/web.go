2eee0097ec254832ff4f5dd3ef75b97770436656
Kobe Nys <kobe@allnice.eu>
2023-04-08 00:09:58 +0200

    Remove fallback for embed.

--- web/web.go
+++ web/web.go
@@ -7,10 +7,8 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/cockroachdb/errors"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
-	"github.com/rs/zerolog/log"
 )
 
 //go:embed build
@@ -19,9 +17,7 @@ var staticFS embed.FS
 // AddRoutes serves the static file system for the UI React App.
 func AddRoutes(router gin.IRouter) {
 	embeddedBuildFolder := newStaticFileSystem()
-	fallbackFS := newFallbackFileSystem(embeddedBuildFolder)
 	router.Use(static.Serve("/", embeddedBuildFolder))
-	router.Use(static.Serve("/", fallbackFS))
 }
 
 // ----------------------------------------------------------------------
@@ -60,31 +56,4 @@ func (s *staticFileSystem) Exists(prefix string, path string) bool {
 		_ = f.Close()
 	}
 	return err == nil
-}
-
-// ----------------------------------------------------------------------
-// fallbackFileSystem wraps a staticFileSystem and always serves /index.html
-type fallbackFileSystem struct {
-	staticFileSystem *staticFileSystem
-}
-
-var _ static.ServeFileSystem = (*fallbackFileSystem)(nil)
-var _ http.FileSystem = (*fallbackFileSystem)(nil)
-
-func newFallbackFileSystem(staticFileSystem *staticFileSystem) *fallbackFileSystem {
-	return &fallbackFileSystem{
-		staticFileSystem: staticFileSystem,
-	}
-}
-
-func (f *fallbackFileSystem) Open(path string) (http.File, error) {
-	file, err := f.staticFileSystem.Open("/index.html")
-	if err != nil {
-		log.Error().Err(err).Msg("Failed to open path \"/index.html\"")
-	}
-	return file, errors.Wrap(err, "Opening path \"/index.html\"")
-}
-
-func (f *fallbackFileSystem) Exists(prefix string, path string) bool {
-	return true
 }
