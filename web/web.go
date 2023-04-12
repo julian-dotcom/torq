bcef075da38b413a4269412a9a7bc0dee05ceb8d
Max Edwards <youwontforgetthis@gmail.com>
2023-04-12 16:05:52 +0100

    refactored embedded filesystem

--- web/web.go
+++ web/web.go
@@ -2,22 +2,12 @@ package web
 
 import (
 	"embed"
-	"fmt"
 	"io/fs"
 	"net/http"
-	"strings"
-
-	"github.com/gin-gonic/contrib/static"
-	"github.com/gin-gonic/gin"
 )
 
 //go:embed build/*
 var staticFS embed.FS
-
-// AddRoutes serves the static file system for the UI React App.
-func AddRoutes(router gin.IRouter) {
-	router.Use(static.Serve("/", newStaticFileSystem()))
-}
 
 // ----------------------------------------------------------------------
 // staticFileSystem serves files out of the embedded build folder
@@ -25,9 +15,7 @@ type staticFileSystem struct {
 	http.FileSystem
 }
 
-var _ static.ServeFileSystem = (*staticFileSystem)(nil)
-
-func newStaticFileSystem() *staticFileSystem {
+func NewStaticFileSystem() *staticFileSystem {
 	sub, err := fs.Sub(staticFS, "build")
 
 	if err != nil {
@@ -37,21 +25,4 @@ func newStaticFileSystem() *staticFileSystem {
 	return &staticFileSystem{
 		FileSystem: http.FS(sub),
 	}
-}
-
-func (s *staticFileSystem) Exists(prefix string, path string) bool {
-	buildpath := fmt.Sprintf("build%s", path)
-
-	// support for folders
-	if strings.HasSuffix(path, "/") {
-		_, err := staticFS.ReadDir(strings.TrimSuffix(buildpath, "/"))
-		return err == nil
-	}
-
-	// support for files
-	f, err := staticFS.Open(buildpath)
-	if f != nil {
-		_ = f.Close()
-	}
-	return err == nil
 }
