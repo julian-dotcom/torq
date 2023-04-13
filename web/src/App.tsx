f53e51ae91765bd1cf738b86ef1a367833c9eaf2
Max Edwards <youwontforgetthis@gmail.com>
2023-04-13 14:48:10 +0100

    moved mixpanel and settings to default layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Router from "./Router";
-import mixpanel from "mixpanel-browser";
 import useTranslations from "./services/i18n/useTranslations";
 import ToastContext from "./features/toast/context";
 import Toasts, { addToastHandle } from "./features/toast/Toasts";
 import styles from "./app.module.scss";
 import navStyles from "features/navigation/nav.module.scss";
-import { useGetSettingsQuery } from "./apiSlice";
-import { Network, selectActiveNetwork } from "features/network/networkSlice";
-import { useAppSelector } from "./store/hooks";
 import LoadingApp from "./components/loading/LoadingApp";
 import { IntercomProvider } from "react-use-intercom";
 import classNames from "classnames";
@@ -17,48 +13,15 @@ function App() {
   const INTERCOM_APP_ID = "y7n3ouse";
   const { init, status: i18nStatus } = useTranslations();
   const toastRef = React.useRef<addToastHandle>();
-  const { data: settingsData } = useGetSettingsQuery();
   const [helpOpen, setHelpOpen] = useState(false);
 
-  const activeNetwork = useAppSelector(selectActiveNetwork);
+  useEffect(() => {
+    init();
+  }, []);
 
   function toggleHelp(open: boolean) {
     setHelpOpen(open);
   }
-
-  useEffect(() => {
-    init();
-    if (process.env.NODE_ENV === "production" && process.env.REACT_APP_E2E_TEST !== "true") {
-      mixpanel.init("f08b3b1c4a2fc9e2c7cc014333cc9233", { ip: false });
-    } else {
-      mixpanel.init("729ace78d0aeb71ba633741d8c92a9ca", { ip: false });
-    }
-  }, []);
-
-  useEffect(() => {
-    if (settingsData) {
-      mixpanel.identify(settingsData.torqUuid);
-      mixpanel.people.set({
-        $opt_out: settingsData.mixpanelOptOut,
-      });
-      mixpanel.people.set_once({
-        $created: new Date().toISOString(),
-      });
-      mixpanel.register({
-        default_date_range: settingsData.defaultDateRange,
-        defaultLanguage: settingsData.defaultLanguage,
-        weekStartsOn: settingsData.weekStartsOn,
-      });
-    }
-  }, [settingsData]);
-
-  useEffect(() => {
-    if (settingsData) {
-      mixpanel.register({
-        network: Network[activeNetwork],
-      });
-    }
-  }, [activeNetwork]);
 
   return i18nStatus === "loading" ? (
     <LoadingApp />
