2aa9945063ab5642b09f3d1e6263310c5320eb5f
ohenrik <oleskogstrom@gmail.com>
2023-04-21 10:07:28 +0200

    fixes any triggered lint error

--- web/src/utils/userEvents.ts
+++ web/src/utils/userEvents.ts
@@ -1,11 +1,12 @@
 import mixpanel from "mixpanel-browser";
 import { useIntercom } from "react-use-intercom";
+import { AnyObject } from "./types";
 
 export const userEvents = () => {
   const { trackEvent } = useIntercom();
 
   // Track with both mixpanel and Intercom
-  const track = (eventName: string, properties?: any) => {
+  const track = (eventName: string, properties?: AnyObject) => {
     mixpanel.track(eventName, properties);
     trackEvent(eventName, properties);
   };
